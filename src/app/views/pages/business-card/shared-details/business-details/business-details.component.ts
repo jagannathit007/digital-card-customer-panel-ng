6c548511038e951a8d00c421ffd1ff3c9410ba1b
Jagannath Maharana <jagannath@itfuturz.com>
2025-01-31 20:13:34 +0530

    Business Details

--- src/app/views/pages/business-card/shared-details/business-details/business-details.component.ts
+++ src/app/views/pages/business-card/shared-details/business-details/business-details.component.ts
@@ -1,10 +1,70 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { common } from 'src/app/core/constants/common';
+import { AppStorage } from 'src/app/core/utilities/app-storage';
 
 @Component({
   selector: 'app-business-details',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './business-details.component.html',
   styleUrl: './business-details.component.scss',
 })
-export class BusinessDetailsComponent { }
+export class BusinessDetailsComponent {
+
+  constructor(private storage: AppStorage) { }
+
+
+  profileImage: File | undefined;
+  coverImage: File | undefined;
+
+  form = {
+    company: '',
+    companyEmail: '',
+    companyMobile: '',
+    companyAddress: '',
+    aboutCompany: '',
+    companyURL: '',
+    map: '',
+    businessCardId: '',
+    companySocial: {
+      facebook: '',
+      google: '',
+      twitter: '',
+      linkedIn: '',
+      instagram: '',
+      youtube: ''
+    },
+    message: {
+      whatsApp: '',
+      email: ''
+    }
+  }
+
+  onSubmit = async () => {
+    let formData = new FormData();
+    formData.append('company', this.form.company);
+    formData.append('companyEmail', this.form.companyEmail);
+    formData.append('companyMobile', this.form.companyMobile);
+    formData.append('companyAddress', this.form.companyAddress);
+    formData.append('aboutCompany', this.form.aboutCompany);
+    formData.append('companyURL', this.form.companyURL);
+    formData.append('map', this.form.map);
+    formData.append('businessCardId', this.storage.get(common.BUSINESS_CARD));
+    formData.append('companySocial', JSON.stringify(this.form.companySocial));
+    formData.append('message', JSON.stringify(this.form.message));
+
+    if (this.profileImage) {
+      formData.append('profileImage', this.profileImage, this.profileImage.name);
+    }
+
+    if (this.coverImage) {
+      formData.append('coverImage', this.coverImage, this.coverImage.name);
+    }
+
+    
+
+  }
+}
