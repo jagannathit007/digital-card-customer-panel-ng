c17fab6fdd3e8d4d5196d39909d642eccf5bd528
ChandanPolai <chandanpolai26@gmail.com>
2025-02-08 16:23:21 +0530

    update business-details

--- src/app/views/pages/business-card/shared-details/business-details/business-details.component.ts
+++ src/app/views/pages/business-card/shared-details/business-details/business-details.component.ts
@@ -1,19 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { common } from 'src/app/core/constants/common';
 import { AppStorage } from 'src/app/core/utilities/app-storage';
 import { AuthService } from 'src/app/services/auth.service';
+
+interface SocialMediaLink {
+  name: string;
+  link: string;
+  isVisible?: boolean;
+}
 
 @Component({
   selector: 'app-business-details',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './business-details.component.html',
   styleUrl: './business-details.component.scss',
 })
 export class BusinessDetailsComponent {
+
+  socialMediaList: SocialMediaLink[] = [];
+  newSocialMedia: SocialMediaLink = { name: '', link: '', isVisible: true };
 
   constructor(private storage: AppStorage, public authService: AuthService) { 
     // this.authService.getBusinessCards();
@@ -45,6 +56,38 @@ export class BusinessDetailsComponent {
       email: ''
     }
   }
+
+
+
+  toggleSocialMedia(platform: string) {
+    
+
+  }
+
+  addSocialMedia(){
+    if (this.newSocialMedia.name && this.newSocialMedia.link) {
+      // Add new social media link to the list
+      this.socialMediaList.push({
+        ...this.newSocialMedia,
+        isVisible: true
+      });
+
+      // Update personalDetails.personalSocial if the platform matches
+      const platformName = this.newSocialMedia.name.toLowerCase();
+      if (platformName in this.form.companySocial) {
+        (this.form.companySocial as any)[platformName] = this.newSocialMedia.link;
+      }
+
+      // Reset the form
+      this.newSocialMedia = { name: '', link: '', isVisible: true };
+    }
+
+
+  }
+
+
+
+
 
   onSubmit = async () => {
     let formData = new FormData();
