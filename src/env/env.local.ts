1e1ecc01553e96fa32258adc06293c6e73c69329
payal9781 <payal@itfuturz.com>
2025-04-03 17:17:36 +0530

    env changed

--- src/env/env.local.ts
+++ src/env/env.local.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: 'https://digitalcard.co.in',
+  // baseURL: 'https://digitalcard.co.in',
+  baseURL: 'http://localhost:3100',
   route: 'web',
 };
