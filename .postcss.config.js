c25d4c657d6adeff8ff4cb03c69fd8e59cb33c32
Hitesh Suthar <hitesh@itfuturz.com>
2025-06-04 16:55:03 +0530

    Enable Tailwind CSS by uncommenting directives in styles.scss and updating tailwind.config.js

--- .postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-prefixwrap")(".tailwind-section", { files: ["tailwind"] }),
-    require("postcss-prefixwrap")(".bootstrap-section", {
-      files: ["bootstrap"],
-    }),
-  ],
-};
