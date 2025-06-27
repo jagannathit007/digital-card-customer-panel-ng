9de4f174270244e428d5b2d046d1a0208c27534a
ChandanPolai <chandanpolai26@gmail.com>
2025-06-27 18:30:13 +0530

    feat: Add TaskClock component with HTML, SCSS, and TypeScript files

---
+++ src/app/views/partials/task-managemnt/common-components/task-clock/task-clock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-clock',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-clock.component.html',
+  styleUrl: './task-clock.component.scss'
+})
+export class TaskClockComponent {
+
+}
