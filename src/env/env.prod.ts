6da9ab139eb2b6ad42461ae7a76a87476ea9036e
Hitesh Suthar <hitesh@itfuturz.com>
2025-08-05 14:59:04 +0530

    fix : updating backend link

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   whiteLabelName: 'Digital Card',
   parentDomain: 'https://digitalcard.co.in',
-  baseURL: 'https://digitalcard.co.in',
+  baseURL: 'https://task-backend.itfuturz.in',
   imageURL: 'https://flinta.s3.ap-south-1.amazonaws.com/',
   route: 'web',
 };
